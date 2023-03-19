b400d22a9bb4cc18102e437444fd78593f95017d
RedHeadphone <huzaifa.k@somaiya.edu>
2023-03-19 18:51:50 +0530

    metadata fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,39 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Codeforces Readme Stats</title>
+        <meta name="title" content="Codeforces Readme Stats"/>
+        <meta
+          name="description"
+          content="⚡ Dynamically generated Codeforces stats for your Github profile!"
+        />
+        <meta name="keywords" content="codeforces, competitive programming, readme stats, contest, programming"/>
+        <meta name="subject" content="Codeforces Readme Stats"/>
+        <meta name="topic" content="Codeforces Readme Stats"/>
+        <meta name="summary" content="An API that generates beautiful statistics of your Codeforces profile as an SVG image, perfect for showcasing your competitive programming skills and achievements on Github."/>
+        <meta name="url" content="https://codeforces-readme-stats.vercel.app/"/>
+        <meta name="category" content="tool"/>
+        <meta name="author" content="Huzaifa Khilawala"/>
+
+        <meta property="og:title" content="Codeforces Readme Stats" />
+        <meta property="og:description" content="⚡ Dynamically generated Codeforces stats for your Github profile!"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://codeforces-readme-stats.vercel.app/" />
+        <meta property="og:image" content="/bg.png" />
+        <meta property="og:image:alt" content="Codeforces Readme Stats" />
+
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="codeforces-readme-stats.vercel.app"/>
+        <meta property="twitter:url" content="https://codeforces-readme-stats.vercel.app/"/>
+        <meta name="twitter:title" content="Codeforces Readme Stats"/>
+        <meta name="twitter:description" content="⚡ Dynamically generated Codeforces stats for your Github profile!"/>
+        <meta name="twitter:image" content="/bg.png"/>
+        <meta name="twitter:image:alt" content="Codeforces Readme Stats"/>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Head from "next/head";
 import Image from "next/image";
 
 import { GithubFilled, BookFilled, ExportOutlined } from '@ant-design/icons';
@@ -48,39 +47,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Codeforces Readme Stats</title>
-        <meta name="title" content="Codeforces Readme Stats"/>
-        <meta
-          name="description"
-          content="⚡ Dynamically generated Codeforces stats for your Github profile!"
-        />
-        <meta name="keywords" content="codeforces, competitive programming, readme stats, contest, programming"/>
-        <meta name="subject" content="Codeforces Readme Stats"/>
-        <meta name="topic" content="Codeforces Readme Stats"/>
-        <meta name="summary" content="An API that generates beautiful statistics of your Codeforces profile as an SVG image, perfect for showcasing your competitive programming skills and achievements on Github."/>
-        <meta name="url" content="https://codeforces-readme-stats.vercel.app/"/>
-        <meta name="category" content="tool"/>
-        <meta name="author" content="Huzaifa Khilawala"/>
-
-        <meta property="og:title" content="Codeforces Readme Stats" />
-        <meta property="og:description" content="⚡ Dynamically generated Codeforces stats for your Github profile!"/>
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://codeforces-readme-stats.vercel.app/" />
-        <meta property="og:image" content="/bg.png" />
-        <meta property="og:image:alt" content="Codeforces Readme Stats" />
-
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:domain" content="codeforces-readme-stats.vercel.app"/>
-        <meta property="twitter:url" content="https://codeforces-readme-stats.vercel.app/"/>
-        <meta name="twitter:title" content="Codeforces Readme Stats"/>
-        <meta name="twitter:description" content="⚡ Dynamically generated Codeforces stats for your Github profile!"/>
-        <meta name="twitter:image" content="/bg.png"/>
-        <meta name="twitter:image:alt" content="Codeforces Readme Stats"/>
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Space className="main-body">
         <Card className="card">
           <Col className="card-col">
